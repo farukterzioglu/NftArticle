22c3698e4c75d51eabea8ce888e8589c9d7d17f2
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2022-01-23 20:40:43 +0300

    Set & get baseurl

--- scripts/nft.ts
+++ scripts/nft.ts
@@ -12,3 +12,30 @@ task("DeployNft", "")
 
         console.log(`Deployed to: ${deployed.address}`);
     });
+
+
+task("GetBaseUri", "")
+    .addParam("contract", "Contract Hash")
+    .setAction(async (taskArgs, hre) => {
+        const contractFactory = await hre.ethers.getContractFactory("MoviePosters");
+        const contractAttached = contractFactory.attach(taskArgs.contract);
+    
+        const baseUri = await contractAttached.getBaseURI();
+    
+        console.log(`Base uri: ${baseUri}`);
+    });
+
+task("SetBaseUri", "")
+    .addParam("contract", "Contract Hash")
+    .addParam("baseurl", "Base Url")
+    .setAction(async (taskArgs, hre) => {
+        const contractFactory = await hre.ethers.getContractFactory("MoviePosters");
+        const contractAttached = contractFactory.attach(taskArgs.contract);
+
+        const tx = await contractAttached.setBaseURI(taskArgs.baseurl);
+        await tx.wait();
+    
+        const baseUri = await contractAttached.getBaseURI();
+        console.log(`New base uri: ${baseUri}`);
+    });
+    
